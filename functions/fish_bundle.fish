46e6cc366c706cadfcd0cb055560b4b46bb1fff3
Bernardo Heynemann <heynemann@gmail.com>
2014-08-18 00:01:00 -0300

    Fixed function name

--- functions/fish_bundle.fish
+++ functions/fish_bundle.fish
@@ -1,3 +1,3 @@
-function FishBundle --description="Adds a bundle to be downloaded by fish-bundles"
+function fish_bundle --description="Adds a bundle to be downloaded by fish-bundles"
     let __fish_bundles_list $__fish_bundles_list $argv
 end
