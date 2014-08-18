6b649879bf66a02815969dfa1e3022e385cb1cf0
Bernardo Heynemann <heynemann@gmail.com>
2014-08-18 00:12:45 -0300

    set not let, jeez

--- functions/fish_bundle.fish
+++ functions/fish_bundle.fish
@@ -1,3 +1,3 @@
 function fish_bundle --description="Adds a bundle to be downloaded by fish-bundles"
-    let __fish_bundles_list $__fish_bundles_list $argv
+    set -gx __fish_bundles_list $__fish_bundles_list $argv
 end
